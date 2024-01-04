2145940b3745f0e2f6a0c854a059ff7e0b734749
savindaJ <your-github-thantrige32@gmail.com.noreply.github.com>
2024-01-04 08:41:26 +0530

    Add connection !

---
+++ Backend/db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "80221474",
+    insecureAuth : true,
+    database:"thogakade"
+});
+
+
+con.connect((err)=>{
+    if (err) throw err;
+    console.log("Connected!");
+})
+
+module.exports = con;
--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,5 +1,6 @@
 const app = require('../app');
 const router = require('../router/router');
+const con = require('../db/connection')
 
 const server = app.listen(3001,'127.0.0.1',()=>{
     console.log("server Started !")
